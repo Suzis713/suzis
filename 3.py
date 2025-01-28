0d81fbcb2e574c6387fc7340c3f708d2fdb044d0
Suzis713 <suzanna.gredzena@inbox.lv>
2025-01-28 09:08:53 +0000

    taisnlenka trijsturs.py

--- 3.py
+++ 3.py
@@ -4,8 +4,7 @@ Ax=int(input("ievadiet Ax koord sakumpunktu"))
 Ay=int(input("ievadiet Ay koord sakumpunktu"))
 Bx=int(input("ievadiet Bx koord sakumpunktu"))
 By=int(input("ievadiet By koord sakumpunktu"))
-Cx=int(input("ievadiet Cx koord sakumpunktu"))
-Cy=int(input("ievadiet Cy koord sakumpunktu"))
+
 
 attalums=sqrt((Ax-Bx)**2+(Ay-By)**2)
 print(attalums)
